e2d3154de661da92ebc08549797f941b3b22b977
Timothy Hahn <timyhahn@gmail.com>
2014-04-12 16:43:30 -0400

    Initial commit

---
+++ src/net/timothyhahn/Buzzed/BuzzedMain.java
@@ -0,0 +1,15 @@
+package net.timothyhahn.Buzzed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BuzzedMain extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
